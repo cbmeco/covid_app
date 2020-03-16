1ee4b609000efd241c806906e438268d2b9df539
cbmeco <cbmeco@gmail.com>
2020-03-16 13:28:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ known_infections = 43
 initial_infections = st.sidebar.number_input(
     "Currently Known Regional Infections", value=known_infections, step=10, format="%i"
 )
-current_hosp = st.sidebar.number_input("Currently Hospitalized/Infected COVID-19 Comcast EEs or Deps", value=1, step=1, format="%i")
+#current_hosp = st.sidebar.number_input("Currently Hospitalized/Infected COVID-19 Comcast EEs or Deps", value=1, step=1, format="%i")
 doubling_time = st.sidebar.number_input(
     "Doubling Time (days)", value=6, step=1, format="%i"
 )
@@ -42,7 +42,7 @@ hosp_los = st.sidebar.number_input("Hospital LOS", value=7, step=1, format="%i")
 #vent_los = st.sidebar.number_input("Vent LOS", value=10, step=1, format="%i")
 Penn_market_share = (
     st.sidebar.number_input(
-        "Comcast EEs + Dependents as Share of Regional Pop (%)", 0.000, 100.000, value=1.000, step=0.001, format="%f"
+        "Comcast EEs + Dependents as Share of Regional Pop (%)", 0.000, 100.000, value=0.008, step=0.001, format="%f"
     )
     / 100.0
 )
@@ -50,7 +50,8 @@ S = st.sidebar.number_input(
     "Regional Population", value=S_default, step=100000, format="%i"
 )
 
-total_infections = current_hosp / Penn_market_share / hosp_rate
+total_infections = #current_hosp /
+Penn_market_share / hosp_rate
 detection_prob = initial_infections / total_infections
 
 st.title("COVID-19 Hospital Impact Model for Epidemics: Adapted for use by Comcast Pop Health")
